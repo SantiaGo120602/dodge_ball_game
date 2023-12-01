a3d4f7ffd7c07fe17549636067b2b4f132ee778b
Santiago <santyjavivas@hotmail.com>
2023-11-30 21:48:47 -0500

    asdfasdf

--- src/view/GameMenu.java
+++ src/view/GameMenu.java
@@ -1,35 +1,35 @@
 package src.view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class GameMenu extends JFrame {
 
     public GameMenu() {
         setTitle("ImageButton Frame");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 800);
+        setSize(1920, 980);
 
         // Create buttons with PNGs
-        ImageButton button1 = new ImageButton("/home/santiago/Documents/patrones1/dodge_ball_game/src/resources/ui_components/PlayButton.png", 100, 50);
-        ImageButton button2 = new ImageButton("/home/santiago/Documents/patrones1/dodge_ball_game/src/resources/ui_components/NewgameButton.png", 100, 50);
-        ImageButton button3 = new ImageButton("/home/santiago/Documents/patrones1/dodge_ball_game/src/resources/ui_components/SettingsButton.png", 100, 50);
-        ImageButton button4 = new ImageButton("/home/santiago/Documents/patrones1/dodge_ball_game/src/resources/ui_components/ExitButton.png", 100, 50);
+        ImageButton button1 = new ImageButton("src/resources/ui_components/PlayButton.png", 100, 50);
+        ImageButton button2 = new ImageButton("src/resources/ui_components/NewgameButton.png", 100, 50);
+        ImageButton button3 = new ImageButton("src/resources/ui_components/SettingsButton.png", 100, 50);
+        ImageButton button4 = new ImageButton("src/resources/ui_components/ExitButton.png", 100, 50);
 
 
-        // Add buttons to a panel
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
-        panel.add(button4);
+        // JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        // panel.setLayout(new FlowLayout());
+        // panel.add(button1);
+        // panel.add(button2);
+        // panel.add(button3);
+        // panel.add(button4);
 
-        // Set the content pane with a background image
-        setContentPane(new JLabel(new ImageIcon("/home/santiago/Documents/patrones1/dodge_ball_game/src/resources/ui_components/background.png")));
+        // add(panel, BorderLayout.CENTER);
+        setContentPane(new JLabel(new ImageIcon("src/resources/ui_components/background.jpg")));
         setLayout(new BorderLayout());
-        add(panel, BorderLayout.SOUTH);
     }
-
 
 }
--- src/view/ImageButton.java
+++ src/view/ImageButton.java
@@ -2,25 +2,20 @@ package src.view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class ImageButton extends JButton {
 
     public ImageButton(String imagePath, int width, int height) {
-        try {
-            System.out.println(imagePath);
-            BufferedImage image = ImageIO.read(getClass().getResource(imagePath));
-            Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            setIcon(new ImageIcon(scaledImage));
-            setBorderPainted(false);
-            setContentAreaFilled(false);
-            setFocusPainted(false);
-            setOpaque(false);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ImageIcon originalIcon = new ImageIcon(imagePath);
+        Image originalImage = originalIcon.getImage();
+        Image resizedImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+        setIcon(resizedIcon);
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setOpaque(false);
+
     }
 }
 
